4a8db071b678fee4755a7550731f09f6edc78f0a
tim-626 <67571878+tim-626@users.noreply.github.com>
2024-02-29 14:54:59 -0600

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,1 +1,7 @@
+## Enable local HTTPS for .NET
+## Trust HTTPS certificate on Linux using Edge or Chrome
+dotnet dev-certs https --trust
 
+## Restore .NET packages and build WebExplorer solution
+cd ./WebExplorer
+dotnet restore
